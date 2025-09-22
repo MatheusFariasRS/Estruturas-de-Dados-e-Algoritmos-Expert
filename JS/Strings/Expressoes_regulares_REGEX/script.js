fe2bc30b4649aeed2ece93026435c0bb3270a615
Matheus Farias <matheusaf.contato@gmail.com>
2025-09-22 10:43:39 -0300

    exemplos REGEX (.br - email)

--- JS/Strings/Expressoes_regulares_REGEX/script.js
+++ JS/Strings/Expressoes_regulares_REGEX/script.js
@@ -18,3 +18,40 @@ function removeNonDigits(string) {
 
 console.log(removeNonDigits("94923784799"));
 console.log(removeNonDigits("213.445.034-82"));
+
+
+//verifique se um dado domínio termina com .br
+
+function validateBrDomain(domain) {
+    const regex = /\.br$/;
+    return regex.test(domain);
+}
+
+console.log(validateBrDomain("batata.com.br")); // true
+console.log(validateBrDomain("banana.com"));  // false
+
+
+//encontre todos endereços de email em um texto.
+
+function findEmails(string) {
+    const regex = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b/g;
+    const result = string.match(regex);
+    if (result) {
+        return result;
+    }
+    return [];
+}
+
+const text = "Para mais informações, contate-nos em contato@exemplo.com ou suporte@exemplo.com.br.";
+
+const emails = findEmails(text);
+
+if (emails.length > 0) {
+    console.log("Emails encontrados:");
+    for (let i = 0; i < emails.length; i++) {
+        console.log(emails[i] + " - " + i);
+    }
+}
+else {
+    console.log("Nenhum email encontrado.")
+}
