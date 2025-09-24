214fa53dd356b7fef22365bae880931893440cb0
Matheus Farias <matheusaf.contato@gmail.com>
2025-09-24 11:10:48 -0300

    dominio email - versao nelio

--- JS/Strings/exercicios/dominio-email/script.js
+++ JS/Strings/exercicios/dominio-email/script.js
@@ -16,3 +16,26 @@ const emailInfo = extractEmailInformation("joao.silva23@yahoo.com.br");
 console.log("Usuario: " + emailInfo.resultUsuario[1]);
 console.log("Dominio: " + emailInfo.resultDominio[1]);
 console.log("Brasileiro: " , emailInfo.resultBrasileiro ? "sim" : "não");
+
+
+//VERSÃO NELIO 
+
+function extractEmailInformation(email) {
+  const parts = email.split("@");
+  const username = parts[0];
+  const domain = parts[1];
+
+  const isBrazilian = domain.endsWith(".br");
+
+  return { username, domain, isBrazilian };
+}
+
+const emailInfo1 = extractEmailInformation("joao.silva23@yahoo.com.br");
+console.log("Usuario:", emailInfo1.username);
+console.log("Dominio:", emailInfo1.domain);
+console.log("Brasileiro:", emailInfo1.isBrazilian ? "sim" : "nao");
+
+const emailInfo2 = extractEmailInformation("maria123@gmail.com");
+console.log("Usuario:", emailInfo2.username);
+console.log("Dominio:", emailInfo2.domain);
+console.log("Brasileiro:", emailInfo2.isBrazilian ? "sim" : "nao");
